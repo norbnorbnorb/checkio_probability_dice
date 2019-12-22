7a148c317291845fd492fbe2824db62296788fac
norbnorbnorb <50047374+norbnorbnorb@users.noreply.github.com>
2019-12-22 13:49:47 +0100

    Update recursion_with_storage.py

--- recursion_with_storage.py
+++ recursion_with_storage.py
@@ -2,7 +2,7 @@
 def test_possibilities(dice_val_sum, dice_left, sides, target, possibility_count, known_outcomes, depth=0):
     known_outcome = known_outcomes.get(dice_left, {}).get(dice_val_sum, None)
     if known_outcome:
-        print(f'outcome known for: d_left: {dice_left}, val: {dice_val_sum}, p: {known_outcome}')
+        # print(f'outcome known for: d_left: {dice_left}, val: {dice_val_sum}, p: {known_outcome}')
         return known_outcome
 
     # base case
@@ -22,18 +22,22 @@ def test_possibilities(dice_val_sum, dice_left, sides, target, possibility_count
             val_too_high = min_reachable > target
             val_too_low = max_reachable < target
 
-            if val_too_high:
+            if val_too_high:  # no need to test higher values
                 break
             if not val_too_low:
                 possibility_count += test_possibilities(
                     dice_val_sum_s, dice_left_s, sides, target, 0, known_outcomes, depth+1
                 )
-                if known_outcomes.get(dice_left_s, None):
-                    known_outcomes[dice_left_s].update({dice_val_sum_s: possibility_count})
-                else:
-                    known_outcomes.update({dice_left_s: {dice_val_sum_s: possibility_count}})
+                # if known_outcomes.get(dice_left_s, None):
+                #     known_outcomes[dice_left_s].update({dice_val_sum_s: possibility_count})
+                # else:
+                #     known_outcomes.update({dice_left_s: {dice_val_sum_s: possibility_count}})
     # print(f'depth: {depth}, p_count: {possibility_count}')
-    print(known_outcomes)
+    # print(known_outcomes)
+    if known_outcomes.get(dice_left, None):
+        known_outcomes[dice_left].update({dice_val_sum: possibility_count})
+    else:
+        known_outcomes.update({dice_left: {dice_val_sum: possibility_count}})
     return possibility_count
 
 
@@ -44,9 +48,9 @@ def probability(dice_number, sides, target):
     known_outcomes = {}  # dice left, sum of values, valid possibilities
 
     valid_possibilities = test_possibilities(dice_val_sum, dice_number, sides, target, possibility_count, known_outcomes)
-    print(f'v: {valid_possibilities}')
+    # print(f'v: {valid_possibilities}')
     probability_chance = round(valid_possibilities / possibilities_total, 4)
-    print(f'p= {probability_chance}')
+    # print(f'p= {probability_chance}')
     return probability_chance
 
 
@@ -57,10 +61,11 @@ if __name__ == '__main__':
         return correct - precision < checked < correct + precision
 
 
-    # assert (almost_equal(probability(2, 6, 3), 0.0556)), "Basic example"
-    # assert (almost_equal(probability(2, 6, 4), 0.0833)), "More points"
-    # assert (almost_equal(probability(2, 6, 7), 0.1667)), "Maximum for two 6-sided dice"
-    # assert (almost_equal(probability(2, 3, 5), 0.2222)), "Small dice"
-    # assert (almost_equal(probability(2, 3, 7), 0.0000)), "Never!"
-    # assert (almost_equal(probability(3, 6, 7), 0.0694)), "Three dice"
+    assert (almost_equal(probability(2, 6, 3), 0.0556)), "Basic example"
+    assert (almost_equal(probability(2, 6, 4), 0.0833)), "More points"
+    assert (almost_equal(probability(2, 6, 7), 0.1667)), "Maximum for two 6-sided dice"
+    assert (almost_equal(probability(2, 3, 5), 0.2222)), "Small dice"
+    assert (almost_equal(probability(2, 3, 7), 0.0000)), "Never!"
+    assert (almost_equal(probability(3, 6, 7), 0.0694)), "Three dice"
     assert (almost_equal(probability(10, 10, 50), 0.0375)), "Many dice, many sides"
+
